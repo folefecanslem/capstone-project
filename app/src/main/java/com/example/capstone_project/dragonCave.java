e8c4a7f27c8666479f12c7f036041d38e1731ced
folefecanslem <folefecanslem@gmail.com>
2023-10-10 23:50:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/capstone_project/dragonCave.java
@@ -0,0 +1,14 @@
+package com.example.capstone_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dragonCave extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dragon_cave);
+    }
+}
