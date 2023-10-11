b62e8995005d862678d064e57b86b430de37ae42
folefecanslem <folefecanslem@gmail.com>
2023-10-11 18:14:09 +0100

    MERGE1

--- app/src/main/java/com/example/capstone_project/MainActivity.java
+++ app/src/main/java/com/example/capstone_project/MainActivity.java
@@ -2,15 +2,57 @@ package com.example.capstone_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button inventory;
+    Button shop;
+    Button cave;
+    Button quit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inventory =  findViewById(R.id.inventory);
+        shop =  findViewById(R.id.shop);
+        cave =  findViewById(R.id.cave);
+        quit =  findViewById(R.id.quit);
+
+
+        inventory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, inventory.class));
+            }
+        });
+
+        shop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, shop.class));
+            }
+        });
+
+        cave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, dragonCave.class));
+            }
+        });
+
+        quit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               System.exit(0);
+            }
+        });
+
 
     }
 
